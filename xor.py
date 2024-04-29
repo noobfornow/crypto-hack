ea3677126e0f1757898a770f8d9df9da58f605cf
noobfornow <farihaanjumaupy@gmail.com>
2024-04-29 18:13:40 +0600

    xor properties

--- xor.py
+++ xor.py
@@ -1,6 +1,11 @@
 from pwn import xor
 
-msg = "label"
-num = 13
+i1 = bytes.fromhex("a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313")
+i2 = bytes.fromhex("37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e")
+i3 = bytes.fromhex("c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1")
+i4 = bytes.fromhex("04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf")
 
-print(xor(msg, num).decode())
+ans = xor(i4, i1, i3)
+
+print(ans)
+
